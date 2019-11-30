06085936af3e0d7188538c32828183fbc313c631
Juan Aleman <juan.j.aleman@gmail.com>
2019-11-30 14:02:09 -0600

    added store and wrapped app in provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
